5276920c156ebc77e370baf26a07f29e56398615
Lynnsey Martin <lmartin@hubspot.com>
2024-12-19 10:56:22 -0700

    [new] add description to Home

--- pandemic-ui/src/js/components/Home.tsx
+++ pandemic-ui/src/js/components/Home.tsx
@@ -1,3 +1,15 @@
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <h2>Welcome to the Pandemic Boardgame!</h2>
+      <p>
+        Pandemic is a cooperative board game where players work together to manage a global
+        pandemic.
+      </p>
+      <p>
+        This is an experimental project - to learn more about it's development or report any issues,
+        see <a href="https://github.com/lemartin19/pandemic">the GitHub repository</a>.
+      </p>
+    </div>
+  );
 }
