1d89cfbee26003217c70e27f0b7dc7b03fbf2804
Lynnsey Martin <lmartin@hubspot.com>
2024-12-20 16:53:10 -0700

    [chore] move border to map

--- pandemic-ui/src/js/app/components/Game.tsx
+++ pandemic-ui/src/js/app/components/Game.tsx
@@ -2,16 +2,11 @@ import '../../../css/Game.css';
 import { Map } from '../../map/components/Map';
 
 import { PlayersSidebar } from '../../players/components/PlayersSidebar';
-import { useCurrentPlayer } from '../../players/hooks/useCurrentPlayer';
 
 export function Game() {
-  const currentPlayer = useCurrentPlayer();
-  const border = currentPlayer?.color && `3px solid ${currentPlayer.color}`;
   return (
-    <div className="Game" style={{ border }}>
-      <div className="GameMapContainer">
-        <Map />
-      </div>
+    <div className="Game">
+      <Map />
       <PlayersSidebar />
     </div>
   );
--- pandemic-ui/src/js/map/components/Map.tsx
+++ pandemic-ui/src/js/map/components/Map.tsx
@@ -1,12 +1,16 @@
 import '../../../css/Map.css';
 
 import { useMapState } from '../../app/store/Map';
+import { useCurrentPlayer } from '../../players/hooks/useCurrentPlayer';
 import { City } from './City';
 
 export function Map() {
+  const currentPlayer = useCurrentPlayer();
+  const border = currentPlayer?.color && `3px solid ${currentPlayer.color}`;
   const { map } = useMapState();
+
   return (
-    <div className="Map">
+    <div className="Map" style={{ border }}>
       <div className="Map-image">
         {map.map((city) => (
           <City key={city.name} city={city} />
