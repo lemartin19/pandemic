db6b56fc55e39250980354d17aba5f07e22dc449
Lynnsey Martin <lmartin@hubspot.com>
2024-12-21 11:10:22 -0700

    [new] draw connections across pacific ocean

--- pandemic-ui/src/js/map/components/Connections.tsx
+++ pandemic-ui/src/js/map/components/Connections.tsx
@@ -2,6 +2,18 @@ import { useState } from 'react';
 import { City } from '../../types/Map';
 import { CITY_POSITIONS } from '../constants/cityPositions';
 
+export function isAcrossPacific(from: string, to: string) {
+  const isBetweenLosAngelesAndSydney =
+    (from === 'Los Angeles' && to === 'Sydney') || (from === 'Sydney' && to === 'Los Angeles');
+  const isBetweenSanFranciscoAndManila =
+    (from === 'San Francisco' && to === 'Manila') || (from === 'Manila' && to === 'San Francisco');
+  const isBetweenSanFranciscoAndTokyo =
+    (from === 'San Francisco' && to === 'Tokyo') || (from === 'Tokyo' && to === 'San Francisco');
+  return (
+    isBetweenLosAngelesAndSydney || isBetweenSanFranciscoAndManila || isBetweenSanFranciscoAndTokyo
+  );
+}
+
 function Connection({
   from,
   to,
@@ -15,11 +27,31 @@ function Connection({
 }) {
   const fromPosition = CITY_POSITIONS[from as keyof typeof CITY_POSITIONS];
   const toPosition = CITY_POSITIONS[to as keyof typeof CITY_POSITIONS];
-  const x1 = (fromPosition.x / 100) * size.width;
-  const y1 = (fromPosition.y / 100) * size.height;
-  const x2 = (toPosition.x / 100) * size.width;
-  const y2 = (toPosition.y / 100) * size.height;
-  return <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth={2} />;
+  const fromX = (fromPosition.x / 100) * size.width;
+  const fromY = (fromPosition.y / 100) * size.height;
+  const toX = (toPosition.x / 100) * size.width;
+  const toY = (toPosition.y / 100) * size.height;
+
+  if (isAcrossPacific(from, to)) {
+    const leftCity = fromX < toX ? { x: fromX, y: fromY } : { x: toX, y: toY };
+    const rightCity = fromX < toX ? { x: toX, y: toY } : { x: fromX, y: fromY };
+    const slope = (rightCity.y - leftCity.y) / (rightCity.x - leftCity.x);
+    const midY = leftCity.y + slope * leftCity.x;
+    return (
+      <>
+        <line x1={0} y1={midY} x2={leftCity.x} y2={leftCity.y} stroke={color} strokeWidth={2} />
+        <line
+          x1={rightCity.x}
+          y1={rightCity.y}
+          x2={size.width}
+          y2={midY}
+          stroke={color}
+          strokeWidth={2}
+        />
+      </>
+    );
+  }
+  return <line x1={fromX} y1={fromY} x2={toX} y2={toY} stroke={color} strokeWidth={2} />;
 }
 
 export function Connections({ city, highlight }: { city: City; highlight: boolean }) {
