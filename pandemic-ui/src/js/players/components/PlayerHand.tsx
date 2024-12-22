da3e224d2b1a0e005bb176ece7c5292589fa2c92
Lynnsey Martin <lmartin@hubspot.com>
2024-12-22 15:34:57 -0700

    [chore] style cards

--- pandemic-ui/src/js/players/components/PlayerHand.tsx
+++ pandemic-ui/src/js/players/components/PlayerHand.tsx
@@ -2,19 +2,27 @@ import { useCurrentGameplayState } from '../../app/store/GamePlayQueue';
 import { CityCard, EventCard, isEventCard } from '../../types/Card';
 import { Deck } from '../../types/Deck';
 
+function Card({ card }: { card: CityCard | EventCard }) {
+  const backgroundColor = isEventCard(card) ? 'green' : card.color;
+  return (
+    <div className="PlayerHand-card" style={{ backgroundColor }}>
+      {card.name}
+    </div>
+  );
+}
+Card.displayName = 'Card';
+
 export function PlayerHand({ hand }: { hand: Deck<CityCard | EventCard> }) {
   const gameplay = useCurrentGameplayState();
   const canInterrupt = hand.some(
     (card) => isEventCard(card) && card.allowedIn.includes(gameplay.type)
   );
   return (
-    <div className="player-hand">
+    <div className="PlayerHand">
       {hand.map((card) => (
-        <div key={card.name} className="player-hand-card">
-          {card.name}
-        </div>
+        <Card key={card.name} card={card} />
       ))}
-      {canInterrupt && <button className="player-events-interrupt">Use Event Card</button>}
+      {canInterrupt && <button className="PlayerHand-interrupt">Use Event Card</button>}
     </div>
   );
 }
