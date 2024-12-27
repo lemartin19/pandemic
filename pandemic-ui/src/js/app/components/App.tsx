c7e534038c1155524b77b0c9a9528251acedd023
Lynnsey Martin <lmartin@hubspot.com>
2024-12-27 11:20:26 -0500

    [new] add StrictMode

--- pandemic-ui/src/js/app/components/App.tsx
+++ pandemic-ui/src/js/app/components/App.tsx
@@ -1,5 +1,6 @@
 import '../../../css/App.css';
 
+import { StrictMode } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { NewGame } from '../../new-game/components/NewGame';
 import { Provider } from '../store/Provider';
@@ -10,19 +11,21 @@ import { Rules } from './Rules';
 
 export function App() {
   return (
-    <Provider>
-      <Navigation />
-      <div className="App">
-        <main className="App-main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new-game" element={<NewGame />} />
-            <Route path="/rules" element={<Rules />} />
-            <Route path="/game" element={<Game />} />
-          </Routes>
-        </main>
-      </div>
-    </Provider>
+    <StrictMode>
+      <Provider>
+        <Navigation />
+        <div className="App">
+          <main className="App-main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new-game" element={<NewGame />} />
+              <Route path="/rules" element={<Rules />} />
+              <Route path="/game" element={<Game />} />
+            </Routes>
+          </main>
+        </div>
+      </Provider>
+    </StrictMode>
   );
 }
 App.displayName = 'App';
