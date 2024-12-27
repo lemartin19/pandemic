e18bc94ef78899a428e792f12557bc4e04083e65
Lynnsey Martin <lmartin@hubspot.com>
2024-12-27 11:18:45 -0500

    [bug] fix sidebar styling

--- pandemic-ui/src/js/players/components/PlayerControls.tsx
+++ pandemic-ui/src/js/players/components/PlayerControls.tsx
@@ -4,6 +4,7 @@ import { calculateFontColor } from '../../utils/calculateFontColor';
 import { Button } from '../../components/Button';
 import { isEventCard } from '../../types/Card';
 import { useCurrentGameplayState } from '../../app/store/GamePlayQueue';
+import { Tooltip } from '../../components/Tooltip';
 
 export function PlayerControls({ player, children }: PropsWithChildren<{ player: Player }>) {
   const gameplay = useCurrentGameplayState();
@@ -15,7 +16,15 @@ export function PlayerControls({ player, children }: PropsWithChildren<{ player:
       className="PlayerControls"
       style={{ backgroundColor: player.color, color: calculateFontColor(player.color) }}
     >
-      <div className="PlayerControls-name">{player.name}</div>
+      <div className="PlayerControls-info">
+        <div className="PlayerControls-name">{player.name}</div>
+        <div className="PlayerControls-role">
+          <Tooltip text={player.role.description} position="left">
+            {player.role.name}
+          </Tooltip>
+        </div>
+        <div className="PlayerControls-location">{player.currentLocation}</div>
+      </div>
       {children}
       {canInterrupt && (
         <Button variant="secondary" size="small">
